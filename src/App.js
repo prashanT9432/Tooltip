f52f65c13ee410c9a6da3b62a8b4a7b991fbf5ea
prashant <prashant9432@gmail.com>
2024-07-03 12:35:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Tooltip from "./Tooltip";
+import styles from "./App.module.css";
 
 function App() {
+  //state to track hover movements (true or false)
+  const [hover, setHover] = useState(false);
+
+  //state to track tooltip positions (top, right, bottom, left; default is "top")
+  const [position, setPosition] = useState("top");
+
+  //handling mouseOver event -- setting hover to "true" on mouseOver
+  const handleMouseOver = () => {
+    setHover(true);
+  };
+
+  //handling mouseLeave event -- setting hover to "false" on mouseLeave
+  const handleMouseLeave = () => {
+    setHover(false);
+  };
+
+  //setting tooltip position based on button clicked
+  const handleTooltipPos = (pos) => {
+    setPosition(pos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div className={styles.div_buttons}>
+        <button onClick={() => handleTooltipPos("top")}>Top</button>
+        <button onClick={() => handleTooltipPos("right")}>Right</button>
+        <button onClick={() => handleTooltipPos("bottom")}>Bottom</button>
+        <button onClick={() => handleTooltipPos("left")}>Left</button>
+      </div>
+      <div className={styles.div_container}>
+        <div
+          className={styles.div_message}
+          onMouseOver={handleMouseOver}
+          onMouseLeave={handleMouseLeave}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <button >Hover over me.</button>
+        </div>
+
+        {/* conditional rendering of Tooltip component depending on 'hover' value */}
+        {/* passing down 'position' as prop to Tooltip component  */}
+
+        {hover && <Tooltip position={position} />}
+      </div>
+    </>
   );
 }
 
---
+++ src/Tooltip.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "./tooltip.module.css";
+ 
+export default function Tooltip(props) {
+  let tooltipPos = '';
+
+//  creating tooltipPos class depending on tooltip 'position' 
+  if (props.position === "top") {
+    tooltipPos = styles.top;
+  } else if (props.position === "right") {
+    tooltipPos = styles.right;
+  } else if (props.position === "bottom") {
+    tooltipPos = styles.bottom;
+  } else {
+    tooltipPos = styles.left;
+  }
+
+  return (
+    // applying 2 classes here -- one for styling (.div_tooltip) and one for tooltip position (tooltipPos)
+    <div className={styles.div_tooltip + ' ' + tooltipPos} >
+      <p>Thanks for hovering! I'm a tooltip</p>
+    </div>
+  );
+}
